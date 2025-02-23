d4532b11bedbc22727c3a7f580aeb685525e3f02
Rey-Gidds <reygidwani2006@gmail.com>
2025-02-23 15:41:30 +0530

    Mistake fixed for the typing bubble flag at the backend

--- index.js
+++ index.js
@@ -78,10 +78,10 @@ io.on('connection' , user => {
         })
 
         user.on('typing' , () => {
-            isBubble[roomKey] = true
             let userColor = rooms[roomKey][user.id]
             let activeBubble = isBubble[roomKey]
             io.to(roomKey).emit('addtypingball' , userColor , activeBubble)
+            isBubble[roomKey] = true
         })
 
         user.on('stoppedTyping' , () => {
