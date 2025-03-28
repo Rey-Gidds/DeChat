cde41dc1ce3833e5191d6588c89a001a73b7abb3
Rey-Gidds <reygidwani2006@gmail.com>
2025-03-28 20:12:42 +0530

    PWA added to the website to allow the users to download the website as an app and use it . Service worker  added and also the manifest and icons of 192x192 and 512x512 added to the icons folder.

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+    console.log('[Service Worker] Installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+// You can cache static assets if needed here
+});
+  
